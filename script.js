36381deb12167a5b514386a8168117235ba1c4ee
jgarcia7-byte <jgarcia7@scu.edu>
2026-02-19 05:53:38 -0800

    Set up map with OpenStreetMap tiles and PNMs markers
    Initialize map and add markers for PNMs.

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ function sayHello() {
   alert("Welcome to the PNM Class Website!");
 }
 
+//This part of the code is for the Map
+const map = L.map('map').setView([37.3,-121.9], 6);
+
+// Add map background (tiles)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 18,
+}).addTo(map);
+
+// Adds a pin for each PNM
+pnms.forEach(pnm => {
+  if (pnm.coords) {
+    L.marker(pnm.coords)
+      .addTo(map)
+      .bindPopup("<b>" + pnm.name + "</b><br>" + pnm.hometown);
+  }
+});
+
 //This part of the is for the Find Your Twin Section
 function findTwin() {
   const userAnswers = {
