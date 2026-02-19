136494596e87ba8c47038e2b09d65737d0f9d2e3
jgarcia7-byte <jgarcia7@scu.edu>
2026-02-19 05:25:06 -0800

    Add 2 pnm data to pnmData.js
    This the questions asked in the google form sent to all PNM's

---
+++ pnmData.js
@@ -0,0 +1,52 @@
+const pnms = [
+  {
+    name: "Lucas Cariquitan",
+    hometown: "San Diego, CA",
+    major: "MECH",
+    gradYear: "2029",
+    favMusic: "Tyler the Creator",
+    movie: "Spiderman Into the Spiderverse",
+    surprise: "I gave a TED Talk that can be found on YouTube",
+    themeSong: "Upside Down by Jack Johnson",
+
+    twinAnswers: {
+      snack: "Sweet",
+      freeFood: "Non chalant walk over",
+      season: "Summer",
+      lifestyle: "Going Out",
+      stress: "Gym/activity",
+      motivation: "Personal Growth",
+      personality: "Trustworthy"
+    }
+  },
+
+  {
+    name: "Brooke Bague",
+    hometown: "Santa Clarita, CA",
+    major: "CENG",
+    gradYear: "2029",
+    favMusic: "The Marias",
+    movie: "New Girl",
+    surprise: "Freddie from iCarly lives in my hometown and I once took a photo with him in the freezer section of Costco. ",
+    themeSong: "After the Storm by Kali Uchis",
+
+    twinAnswers: {
+      snack: "Sweet",
+      freeFood: "Non chalant walk over",
+      season: "Fall",
+      lifestyle: "Staying In",
+      stress: "Talk with friends or family",
+      motivation: "Personal Growth",
+      personality: "Trustworthy"
+    }
+  },
+
+  {
+    //insert new pnm here
+
+    //insert pnm twin answers
+  }
+  },
+  
+  {
+]; //the ending of the code after all pnm are inserted
