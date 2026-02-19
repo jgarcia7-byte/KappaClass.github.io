525b8e7f3528462676977c7c0d4b7a49ca83c0de
jgarcia7-byte <jgarcia7@scu.edu>
2026-02-19 05:40:41 -0800

    Add findTwin function to match user answers

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 function sayHello() {
   alert("Welcome to the PNM Class Website!");
 }
+
+function findTwin() {
+  const userAnswers = {
+    snack: document.getElementById("snack").value,
+    freeFood: document.getElementById("freeFood").value,
+    season: document.getElementById("season").value
+  };
+
+  let bestMatch = null;
+  let highestScore = -1;
+
+  pnms.forEach(pnm => {
+    let score = 0;
+
+    if (pnm.twinAnswers.snack === userAnswers.snack) score++;
+    if (pnm.twinAnswers.freeFood === userAnswers.freeFood) score++;
+    if (pnm.twinAnswers.season === userAnswers.season) score++;
+
+    if (score > highestScore) {
+      highestScore = score;
+      bestMatch = pnm;
+    }
+  });
+
+  document.getElementById("result").innerHTML = "Your Twin is <b>" + bestMatch.name + "</b> from " + bestMatch.hometown;
+}
