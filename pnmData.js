9ca4bda10c7b8b632b35b2ae535634ad04b7f1a1
jgarcia7-byte <jgarcia7@scu.edu>
2026-02-20 02:41:03 -0800

    Add new PNM entries with details and media links

--- pnmData.js
+++ pnmData.js
@@ -5,10 +5,11 @@ const pnms = [
     coords: [32.7157, 117.1611], // [Latitude, Longitude]
     major: "MECH",
     gradYear: "2029",
+    themeSong: "https://www.youtube.com/watch?v=dqUdI4AIDF0",
+    image: "https://drive.google.com/file/d/1Roc1ILPIAjlN9meP76FsvdbydHbnoipb/view?usp=sharing",
     favMusic: "Tyler the Creator",
     movie: "Spiderman Into the Spiderverse",
     surprise: "I gave a TED Talk that can be found on YouTube",
-    themeSong: "Upside Down by Jack Johnson",
 
     twinAnswers: {
       snack: "Sweet",
@@ -27,10 +28,11 @@ const pnms = [
     coords: [34.39, 118.54], // [Latitude, Longitude]
     major: "CENG",
     gradYear: "2029",
+    themeSong: "https://www.youtube.com/watch?v=9f5zD7ZSNpQ&list=RD9f5zD7ZSNpQ&start_radio=1",
+    image: "https://drive.google.com/file/d/1eeZDighBidRZrOiHJzpcDBW7l5WpKYAi/view?usp=sharing",
     favMusic: "The Marias",
     movie: "New Girl",
     surprise: "Freddie from iCarly lives in my hometown and I once took a photo with him in the freezer section of Costco. ",
-    themeSong: "After the Storm by Kali Uchis",
 
     twinAnswers: {
       snack: "Sweet",
@@ -44,11 +46,279 @@ const pnms = [
   },
 
   {
-    //insert new pnm here
+    name: "Ethan Pessoa Jinian",
+    hometown: "San Francisco, CA",
+    coords: [37.77, 122.42], // [Latitude, Longitude]
+    major: "MECH",
+    gradYear: "2029",
+    themeSong: "https://www.youtube.com/watch?v=VlTEQ4p2xDM&list=RDVlTEQ4p2xDM&start_radio=1",
+    image: "https://drive.google.com/file/d/1wQF-kPpvVUuMrvPpL-F-KQoe_6CXeI3g/view?usp=sharing",
+    favMusic: "Daniel Caesar",
+    movie: "American Psycho",
+    surprise: "I love Cars",
 
-    //insert pnm twin answers
+    twinAnswers: {
+      snack: "Salty",
+      freeFood: "Im not even that hungry",
+      season: "Summer",
+      lifestyle: "Adventure/Exploring",
+      stress: "Gym/Activity",
+      motivation: "Personal Growth",
+      personality: "Ambitious"
+    }
+  },
+
+  {
+    name: "Colin Esperanza",
+    hometown: "Napa, CA",
+    coords: [38.3, 122.29], // [Latitude, Longitude]
+    major: "CSEN",
+    gradYear: "2028",
+    themeSong: "https://www.youtube.com/watch?v=iGbeZNqklic&list=RDiGbeZNqklic&rco=1",
+    image: "https://drive.google.com/file/d/1nu9oZxj_HaQ4k5DPhp_vdYx5zLxzngjM/view?usp=sharing",
+    favMusic: "Laufey / RnB",
+    movie: "RuPaul’s Drag Race",
+    surprise: "I ate fertilizer once as a kid",
+
+    twinAnswers: {
+      snack: "Whatever is there",
+      freeFood: "Take extra for later",
+      season: "Summer",
+      lifestyle: "Staying in",
+      stress: "Talk with friends or family",
+      motivation: "Personal Growth",
+      personality: "Funny"
   }
   },
-  
+
+  {
+    name: "Lyra Kapadia",
+    hometown: "San Ramon, CA",
+    coords: [37.78, 121.98], // [Latitude, Longitude]
+    major: "ECEN",
+    gradYear: "2029",
+    themeSong: "N/A",
+    image: "https://drive.google.com/file/d/16n4X_Ye1YKXxkwovw9tpoKFYrh0z6xk6/view?usp=sharing",
+    favMusic: "Lana Del Rey",
+    movie: "Modern Family",
+    surprise: "I love traveling and have visited 18 countries so far!",
+
+    twinAnswers: {
+      snack: "Both",
+      freeFood: "Non chalant walk over",
+      season: "Summer",
+      lifestyle: "Adventure / Exploring",
+      stress: "Talk with friends or family",
+      motivation: "Personal Growth",
+      personality: "Ambitious"
+  }
+  },
+
+  {
+    name: "Sarah Gupte",
+    hometown: "Pleasanton, CA",
+    coords: [37.66, 121.88], // [Latitude, Longitude]
+    major: "CSEN",
+    gradYear: "2028",
+    themeSong: "https://www.youtube.com/watch?v=RSsTx2TBrww&list=RDRSsTx2TBrww&start_radio=1",
+    image: "https://drive.google.com/file/d/1fdVUzZNqHSwnjI_3i8vf6kZ1fcFxloGF/view?usp=sharing",
+    favMusic: "Everything but metal",
+    movie: "Mulan",
+    surprise: "I had a mullet",
+
+    twinAnswers: {
+      snack: "Both",
+      freeFood: "Non chalant walk over",
+      season: "Summer",
+      lifestyle: "Adventure / Exploring",
+      stress: "Gym / Activity",
+      motivation: "Fun / experiences",
+      personality: "Trustworthy"
+  }
+  },
+
+    {
+    name: "Astha Desai",
+    hometown: "Fremont, CA",
+    coords: [37.55, 121.99], // [Latitude, Longitude]
+    major: "CSEN",
+    gradYear: "2028",
+    themeSong: "https://www.youtube.com/watch?v=19VAnMSf6o0&list=RD19VAnMSf6o0&start_radio=1",
+    image: "https://drive.google.com/file/d/1IXS7C5x0npu_s7XmdGG0bZuVd9eMkH3F/view?usp=sharing",
+    favMusic: "Weeknd, Clairo, Chase Atlantic, Lana Del Rey",
+    movie: "10 Things I Hate About You",
+    surprise: "I used to figure skate at a competitive level",
+
+    twinAnswers: {
+      snack: "Salty",
+      freeFood: "Non chalant walk over",
+      season: "Summer",
+      lifestyle: "Adventure / Exploring",
+      stress: "Sleep",
+      motivation: "Personal Growth",
+      personality: "Trustworthy"
+  }
+  },
+
+  {
+    name: "Amy Vu",
+    hometown: "San Jose, CA",
+    coords: [37.3387, 121.8853], // [Latitude, Longitude]
+    major: "CSEN",
+    gradYear: "2029",
+    themeSong: "https://www.youtube.com/watch?v=-xoxu1zHiSY&list=RD-xoxu1zHiSY&start_radio=1",
+    image: "https://drive.google.com/file/d/11rgEadEmNGc5ylFX61gEMrU0uUUg4m5H/view?usp=sharing",
+    favMusic: "Laufey / Pop",
+    movie: "White Chicks",
+    surprise: "I once had a orange/red hair dye phase",
+
+    twinAnswers: {
+      snack: "Both",
+      freeFood: "Non chalant walk over",
+      season: "Spring",
+      lifestyle: "Staying In",
+      stress: "Talk with friends or family",
+      motivation: "Fun / experiences",
+      personality: "Loyal"
+  }
+  },
+
+  {
+    name: "Marisol Roeck",
+    hometown: "Lake Forest, IL",
+    coords: [42.26, 87.84], // [Latitude, Longitude]
+    major: "MECH",
+    gradYear: "2029",
+    themeSong: "https://www.youtube.com/watch?v=5igDtWadYms&list=RD5igDtWadYms&start_radio=1",
+    image: "https://drive.google.com/file/d/1kDVkJCAVUN7OCAiqd5aMCtP0JWZL4ucP/view?usp=sharing",
+    favMusic: "Taylor Swift/Yacht Rock",
+    movie: "Pitch Perfect",
+    surprise: "I love dad music",
+
+    twinAnswers: {
+      snack: "Sweet",
+      freeFood: "Non chalant walk over",
+      season: "Summer",
+      lifestyle: "Going Out",
+      stress: "Talk with friends or family",
+      motivation: "Fun / experiences",
+      personality: "Funny"
+  }
+  },
+
+  {
+    name: "Daniela Diaz",
+    hometown: "San Jose, CA",
+    coords: [37.3387, 121.8853], // [Latitude, Longitude]
+    major: "WDE",
+    gradYear: "2028",
+    themeSong: "https://www.youtube.com/watch?v=-HLGCKkDoPw&list=RD-HLGCKkDoPw&start_radio=1",
+    image: "https://drive.google.com/file/d/1ApIWz33hE_sgj050TPqtVzIQjaJvIuHg/view?usp=sharing",
+    favMusic: "R&B & Hip Hop",
+    movie: "White Chicks",
+    surprise: "I used to run track and field",
+
+    twinAnswers: {
+      snack: "Sweet",
+      freeFood: "First in line",
+      season: "Spring",
+      lifestyle: "Adventure / Exploring",
+      stress: "Cry",
+      motivation: "money",
+      personality: "Ambitious"
+  }
+  },
+
+  {
+    name: "Zoe Costianis",
+    hometown: "Glenview, IL",
+    coords: [42.0746, -87.8116], // [Latitude, Longitude]
+    major: "BIOE",
+    gradYear: "2028",
+    themeSong: "https://www.youtube.com/watch?v=RB-RcX5DS5A&list=RDRB-RcX5DS5A&start_radio=1",
+    image: "https://drive.google.com/file/d/1mHZ0fwvnDNF4qQRJe6otwuqJ6jG-YF4Q/view?usp=sharing",
+    favMusic: "Noah Kahan/Radiohead ",
+    movie: "Mamma Mia/Arrested Development",
+    surprise: "I am OSHA forklift certified",
+
+    twinAnswers: {
+      snack: "Salty",
+      freeFood: "Non chalant walk over",
+      season: "Spring",
+      lifestyle: "Adventure / Exploring",
+      stress: "Talk with friends or family",
+      motivation: "Fun / experiences",
+      personality: "Ambitious"
+  }
+  },
+
+  {
+    name: "Mia Buccino",
+    hometown: "Covina, CA",
+    coords: [34.09, -117.89], // [Latitude, Longitude]
+    major: "ENGRUD",
+    gradYear: "2029",
+    themeSong: "https://www.youtube.com/watch?v=yZE937z9e9g&list=RDyZE937z9e9g&start_radio=1",
+    image: "https://drive.google.com/file/d/1x3pOqz2ZaOLEeJjAzZtu15K73oobN_jc/view?usp=sharing",
+    favMusic: "r&b, jazz, rock",
+    movie: "Ratatouille, Breaking Bad",
+    surprise: "I play the piano",
+
+    twinAnswers: {
+      snack: "Both",
+      freeFood: "Take extra for later",
+      season: "Spring",
+      lifestyle: "Mix of everything",
+      stress: "Talk with friends or family",
+      motivation: "Personal Growth",
+      personality: "Trustworthy"
+  }
+  },
+
+  {
+    name: "Jaden Tran",
+    hometown: "San Jose, CA",
+    coords: [37.3387, 121.8853], // [Latitude, Longitude]
+    major: "ECEN",
+    gradYear: "2028",
+    themeSong: "https://www.youtube.com/watch?v=4uFalk1y38I&list=RD4uFalk1y38I&start_radio=1",
+    image: "https://drive.google.com/file/d/1aKswoeKWfXlTReHfz3cvVVteBUaj7nnu/view?usp=sharing",
+    favMusic: "Daniel Caesar/R&B",
+    movie: "Everybody Hates Chris",
+    surprise: "I've never been to a concert before",
+
+    twinAnswers: {
+      snack: "Salty",
+      freeFood: "Non chalant walk over",
+      season: "Summer",
+      lifestyle: "Staying In",
+      stress: "Talk with friends or family",
+      motivation: "Fun / experiences",
+      personality: "Trustworthy"
+  }
+  },
+
+  {
+    name: "Jonathan Garcia",
+    hometown: "San Jose, CA",
+    coords: [37.3387, 121.8853], // [Latitude, Longitude]
+    major: "ECEN",
+    gradYear: "2028",
+    themeSong: "https://www.youtube.com/watch?v=pildU9lK6vM",
+    image: "https://drive.google.com/file/d/1EDwZzpETHS3G0XKJG0J4STyWoTCOt9RX/view?usp=drive_link",
+    favMusic: "Travis Scott, Kendrick Lamar, Don Toliver",
+    movie: "The Sandlot",
+    surprise: "N/A",
+
+    twinAnswers: {
+      snack: "Both",
+      freeFood: "First in line",
+      season: "Spring",
+      lifestyle: "Adventure / Explore",
+      stress: "Gym / Activity",
+      motivation: "Family",
+      personality: "Loyal"
+  }
+  },
   {
 ]; //the ending of the code after all pnm are inserted
